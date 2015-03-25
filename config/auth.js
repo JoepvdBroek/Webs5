07c7b3903651a87bdb147e00c4a3047cffa87a07
Thomas de Brouwer <tjm.debrouwer@student.avans.nl>
2015-03-25 12:21:44 +0100

    first comit

---
+++ config/auth.js
@@ -0,0 +1,22 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '618405501627714', // your App ID
+        'clientSecret'  : 'a709137ae514d73116f308320b6fb3e3', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
