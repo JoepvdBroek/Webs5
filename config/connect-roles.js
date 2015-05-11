08f77a49892df106a7b1590d911f23061fce404c
JoepvdBroek <joep28_12@hotmail.com>
2015-05-11 13:42:51 +0200

    oplossing hopelijk

--- config/connect-roles.js
+++ config/connect-roles.js
@@ -3,30 +3,34 @@ var ConnectRoles = require('connect-roles');
 module.exports = function(){
 	var roles = new ConnectRoles({
   		failureHandler: function (req, res, action) {
-    // optional function to customise code that runs when
-    // user fails authorisation
-    var accept = req.headers.accept || '';
-    res.status(401);
-    if (~accept.indexOf('html')) {
-      //res.render('access-denied', {action: action});
-      res.render('accesdenied.html');
+	    // optional function to customise code that runs when
+	    // user fails authorisation
+		    var accept = req.headers.accept || '';
+		    res.status(401);
+		    if (~accept.indexOf('html')) {
+		      //res.render('access-denied', {action: action});
+		      res.render('accesdenied.html');
 
-    } else {
-      res.send('Access Denied - You don\'t have permission to: ' + action);
-    }
-  }
+		    } else {
+		      res.send('Access Denied - You don\'t have permission to: ' + action);
+		    }
+	  	}
 	});
 
 	roles.use('access beheerder', function (req) {
-		if(!req.user){ return false};
-	  		if(req.user.hasAnyRole('beheerder')){
-	  			console.log("access beheerder: true");
-	  			return true;
-	  		};
+		console.log("access beheerder check");
+		if(!req.user){ 
+			console.log('!req.user');
+			return false;
+		}
+  		if(req.user.hasAnyRole('beheerder')){
+  			console.log("access beheerder: true");
+  			return true;
+  		}
 	});
 
 	roles.use('access normal user', function (req) {
-		if(!req.user){ return false};
+		if(!req.user){ return false}
 		return true;
 	});
 
@@ -34,7 +38,7 @@ module.exports = function(){
 	roles.use(function (req) {
   		if(req.user.hasAnyRole('admin')){
   			return true;
-  		};
+  		}
 	});
 
 	return roles;
--- public/js/backend.js
+++ public/js/backend.js
@@ -1,4 +1,5 @@
 var prefix = "https://webs5restrace.herokuapp.com"
+//var prefix = "http://localhost:8080";
 
 var per_page = 5;
 var currentPage = 1;
